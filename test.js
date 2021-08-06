811d05ddb2b41c21dfc929d76affb7d2bbafc678
Mashael <m.alsaeedi@safcsp.org.sa>
2021-08-06 20:51:12 +0300

    create showUsers

--- test.js
+++ test.js
@@ -1,1 +1,21 @@
+class Test {
+    
+    constructor(users){
+        this.users = users;
+    }
+    
+    showUsers(){
+        let usersNames = [];
+        for (let i = 0; i < this.users.length; i++){
+            usersNames.push(this.users[i].name);
+        }
+        return usersNames;
+    }
+}
 
+
+let test = new Test([
+                     {name: 'Mashael', age: 12},
+                     {name: 'Moahmmed', age: 10}
+                     ]);
+console.log(test.showUsers());
